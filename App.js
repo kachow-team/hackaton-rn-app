d0700f3b7058e19e8245b3425bdede8b35a1945e
xmmt <mkmax472@gmail.com>
2020-09-11 22:33:02 +0300

    еще два экранчика

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -33,10 +33,95 @@ function DonationType({ navigation }) {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
           title="Целевой сбор Когда есть определенная цель"
-          onPress={() => navigation.navigate('Details')}
+          onPress={() => navigation.navigate('TargetDonation')}
         />
         <Button
           title="Регулярный сбор Если помощь нужна ежемесячно"
+          onPress={() => navigation.navigate('RegularDonation')}
+        />
+      </View>
+    );
+}
+
+function TargetDonation({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Загрузить обложку"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Text>Название сбора</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Название сбора"
+        />
+        <Text>Сумма, ₽</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Сколько нужно собрать?"
+        />
+        <Text>Цель</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Например, лечение человека"
+        />
+        <Text>Описание</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="На что пойдут деньги и как они кому-то помогут?"
+        />
+        <Text>Куда получать деньги?</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            value="Счет VK Pay · 1234"
+        />
+        <Button
+          title="Далее"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
+    );
+}
+
+function RegularDonation({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Загрузить обложку"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Text>Название сбора</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Название сбора"
+        />
+        <Text>Сумма, ₽</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Сколько нужно в месяц?"
+        />
+        <Text>Цель</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="Например, поддержка приюта"
+        />
+        <Text>Описание</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="На что пойдут деньги и как они кому-то помогут?"
+        />
+        <Text>Куда получать деньги?</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            value="Счет VK Pay · 1234"
+        />
+        <Text>Автор</Text>
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            value="Матвей Правосудов"
+        />
+        <Button
+          title="Далее"
           onPress={() => navigation.navigate('Details')}
         />
       </View>
@@ -69,6 +154,8 @@ function App() {
       <Stack.Navigator initialRouteName="Donations">
         <Stack.Screen name="Donations" title="Пожертвования" component={Donations} />
         <Stack.Screen name="DonationType" component={DonationType} />
+        <Stack.Screen name="TargetDonation" component={TargetDonation} />
+        <Stack.Screen name="RegularDonation" component={RegularDonation} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
