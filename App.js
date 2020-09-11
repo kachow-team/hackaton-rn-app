6d3a0a8aaaffa82e879140262bf03c1aa4d4f520
xmmt <mkmax472@gmail.com>
2020-09-11 21:51:39 +0300

    еще кнопки

--- App.js
+++ App.js
@@ -16,16 +16,18 @@ function HomeScreen({ navigation }) {
 }
 
 function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-    </View>
-  );
-}
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push('Details')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }
 
 const Stack = createStackNavigator();
 
