98eab8822baf9c353d7e1975d1e7fe1af405e247
Maksim Barkalov <maks@fizmatspb.ru>
2020-09-12 13:24:19 +0300

    пробросил описание

--- App.js
+++ App.js
@@ -136,7 +136,8 @@ class TargetDonation extends React.Component<props> {
   state = {
     donationName: '',
     author: 'user',
-    picSource: null
+    picSource: null,
+      description:''
 };
 updatePicSource = (newSource) => {
   this.setState({picSource: newSource});
@@ -179,6 +180,7 @@ placeholder="Например, лечение человека"
                             numberOfLines={2}
                             style={{...styles.textInput, height: 70, paddingTop:10}}
                             placeholder="На что пойдут деньги и как они кому-то помогут?"
+                            onChangeText={((value)=> this.setState({description:value}))}
                         />
         <Text style={styles.textInputDescription} >
           Куда получать деньги?
@@ -189,7 +191,7 @@ placeholder="Например, лечение человека"
         />
         <TouchableOpacity
             style={{...styles.button, marginBottom:40}}
-            onPress={() => this.props.navigation.navigate('Feed', {author: "Андрей Иванов", donationName: this.state.donationName, picSource: this.state.picSource})}
+            onPress={() => this.props.navigation.navigate('Feed', {author: "Андрей Иванов", donationName: this.state.donationName, picSource: this.state.picSource, description: this.state.description})}
         >
               <NextButton />
         </TouchableOpacity>
@@ -204,7 +206,8 @@ class RegularDonation extends React.Component<props> {
     state = {
         donationName: '',
         author: 'user',
-        picSource: null
+        picSource: null,
+        description:''
     };
 
     updatePicSource = (newSource) => {
@@ -254,6 +257,7 @@ class RegularDonation extends React.Component<props> {
                             numberOfLines={2}
                             style={{...styles.textInput, height: 70, paddingTop:10}}
                             placeholder="На что пойдут деньги и как они кому-то помогут?"
+                            onChangeText={((value)=> this.setState({description:value}))}
                         />
                         <Text style={styles.textInputDescription}>
                             Куда получать деньги?
@@ -297,7 +301,7 @@ class RegularDonation extends React.Component<props> {
                         />
                         <TouchableOpacity
                             style={{...styles.button, marginBottom:40}}
-                            onPress={() => this.props.navigation.navigate('Feed', {author: this.state.author, donationName: this.state.donationName, picSource: this.state.picSource})}
+                            onPress={() => this.props.navigation.navigate('Feed', {author: this.state.author, donationName: this.state.donationName, picSource: this.state.picSource, description: this.state.description})}
                         >
                             <NextButton />
                         </TouchableOpacity>
@@ -331,6 +335,7 @@ function Feed({route, navigation }) {
     const { author } = route.params;
     const { donationName } = route.params;
     const { picSource } = route.params;
+    const { description } = route.params;
     return (
         <View style={{flex:1, backgroundColor:'#FFFFFF'}}>
         <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center', marginLeft:'5.5%', marginRight:'5.5%'}}>
@@ -384,7 +389,7 @@ function Feed({route, navigation }) {
                     <View>
                     <TouchableOpacity
 
-                      onPress={() => navigation.navigate('BigMock', {picSource: picSource, donationName: donationName, author: author})}
+                      onPress={() => navigation.navigate('BigMock', {picSource: picSource, donationName: donationName, author: author, description:description})}
                     >
                     <HelpOutlineButton />
                     </TouchableOpacity>
@@ -401,7 +406,7 @@ function BigMock({ route, navigation }) {
   const { author } = route.params;
     const { donationName } = route.params;
     const { picSource } = route.params;
-    console.log(picSource);
+    const { description } = route.params;
   return (
     <SafeAreaView style={{backgroundColor: '#FFFFFF'}}>
                 <ScrollView style={styles.scrollView}>
@@ -413,7 +418,7 @@ function BigMock({ route, navigation }) {
         <View style={{paddingTop:20, paddingLeft:10}}>
           <Text style={styles.textTitle}>{donationName}</Text>
           <Text style={styles.textAuthor}>Автор {author}</Text>
-          <Text style={styles.textType}>Type</Text>
+          <Text style={styles.textType}>{description}</Text>
         </View>
         <View>
           <PostFooter/>
